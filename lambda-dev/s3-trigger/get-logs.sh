0f5f57d81b49be3c8f6a399c31c0ec367b717ab4
trebsirk <trebsirk@gmail.com>
2024-03-08 00:23:02 -0500

    deleted old folders

--- lambda-dev/s3-trigger/get-logs.sh
+++
@@ -1,1 +0,0 @@
-aws logs get-log-events --log-group-name /aws/lambda/lambda-dev-s3-trigger-demo --log-stream-name 2024/03/05/[\$LATEST]f30d5668fea94de8a65515b0a6da1eca --limit 5
--- lambda-dev/s3-trigger/zip-py.sh
+++
@@ -1,3 +0,0 @@
-mkdir artifacts
-cd code
-zip ../artifacts/lambda_function.zip lambda_function.py requirements.txt
